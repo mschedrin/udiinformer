11d1e89949fef8e6108438e317fc4fd58184895b
Mikhail Shchedrin <mschedrin@gmail.com>
2021-09-27 00:25:04 +0200

    add logger

--- rebook_opphold.py
+++ rebook_opphold.py
@@ -1,15 +1,19 @@
-import tempfile, time
+#!/home/mschedrin/.virtualenvs/udiinformer/bin/python
+import tempfile, time, os, logging
 from calendar import monthrange
 
 import dateparser
 import datetime
 import playwright
-#import playwright.helper
 import playwright.sync_api
 import telegram_send
-#from playwright import sync_playwright
 from playwright.sync_api import sync_playwright
 from pydantic import BaseSettings, SecretStr
+
+FILE_LOG_FORMATTER = logging.Formatter("[%(asctime)s %(name)s|%(module)s:%(lineno)d|%(threadName)s|%(process)d] %(levelname)s %(message)s","%Y-%m-%d %H:%M:%S")
+CONSOLE_LOG_FORMATTER = logging.Formatter("[%(asctime)s %(name)s] %(levelname)-4s %(message)s", "%m.%d %H:%M")
+LOG_LEVEL = logging.DEBUG
+
 
 class Settings(BaseSettings):
     EMAIL: str
@@ -17,7 +21,7 @@ class Settings(BaseSettings):
 
     class Config:
         env_prefix = "UDI_"
-        env_file = ".env"
+        env_file = os.path.dirname(os.path.realpath(__file__))+"/.env"
         env_file_encoding = "utf-8"
 
 
@@ -29,8 +33,21 @@ def send_success(page: playwright.sync_api.Page, msg: str):
         fp.seek(0, 0)
         telegram_send.send(images=[fp])
 
+def setup_logger(name, logFile, level=logging.DEBUG):
+    fileHandler = logging.FileHandler(logFile)
+    fileHandler.setFormatter(FILE_LOG_FORMATTER)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(CONSOLE_LOG_FORMATTER)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(fileHandler)
+    logger.addHandler(consoleHandler)
+    return logger
+
 
 def main():
+    logger = setup_logger("udiinformer", os.path.dirname(os.path.realpath(__file__))+"/udiinformer.log",LOG_LEVEL)
+    logger.info("udiinformer started")
     config = Settings()
     with sync_playwright() as p:
         # browser = p.chromium.launch(headless=False)
@@ -53,9 +70,9 @@ def main():
             # book appointment
             page.click(book_btn_id)
         except Exception as e:
-            print(str(e))
+            logger.info(str(e))
             msg = "Failed to login. Check your password."
-            print(msg)
+            logger.info(msg)
             telegram_send.send(messages=[msg])
             return
 
@@ -68,13 +85,14 @@ def main():
         try:
             page.wait_for_selector(change_btn_id, timeout=5000)
         except playwright.helper.TimeoutError:
-            print("No appointments to rebook.")
+            logger.info("No appointments to rebook.")
             return
 
         current_booking_id = "#ctl00_PageRegion_MainContentRegion_ViewControl_spnReceiptAndBooking_BookingSummaryInfo_lblDate"
         current_booking = page.text_content(current_booking_id)
         current_booking = dateparser.parse(current_booking)
-        #current_booking = dateparser.parse("Wednesday 1 September 2022")
+        # current_booking = dateparser.parse("Wednesday 1 September 2022")
+        logger.info(f"Current booking date { current_booking }")
         page.click(change_btn_id)
         #page.waitForNavigation(waitUntil='load')
         # page.wait_for_load_state('networkidle')
@@ -94,13 +112,13 @@ def main():
             # page.wait_for_load_state()
             view_month_txt = page.query_selector("h2").inner_text()
             view_month = dateparser.parse(view_month_txt)
-            print(f"Checking { view_month_txt }")
+            logger.info(f"Checking { view_month_txt }")
 
             #num_closed = len(page.querySelectorAll('css=[class="bookingCalendarClosedDay"]'))
             # num_closed = len(page.querySelectorAll('css=[class="bookingCalendarClosedDay"]')) + len(page.querySelectorAll('css=[class="bookingCalendarFullyBookedDay"]'))
             # num_days_in_month = monthrange(view_month.year, view_month.month)[1]
             # if num_days_in_month == num_closed:
-            #     print("Reached a fully closed month.")
+            #     logger.info("Reached a fully closed month.")
             #     #break
             #     page.click(next_btn_id)
             #     page.waitForNavigation()
@@ -118,6 +136,7 @@ def main():
                 if bookable_date < current_booking:
                     msg = f"It is possible to rebook the appointment on {bookable_day}, {view_month_txt}!"
                     send_success(page, msg)
+                    success = True
                     break
 
             page.click(next_btn_id)
@@ -127,7 +146,8 @@ def main():
             page.wait_for_load_state(state = "domcontentloaded")
             time.sleep(3)
             
-        if not success: print("No possibilities to rebook.")
+        if not success: logger.info("No possibilities to rebook. Script terminates")
+
 
 
 if __name__ == "__main__":
